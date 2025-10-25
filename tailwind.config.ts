2093909f367e2d310fa0d1f61a6274f9e3e7b796
Mayank <mayanksharma4174@gmail.com>
2025-10-25 19:22:22 +0000

    Suno button click me ye wala animation use karo but is code me modificat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,10 +85,16 @@ export default {
             height: '0',
           },
         },
+        glowing: {
+          '0%': { 'background-position': '0 0' },
+          '50%': { 'background-position': '400% 0' },
+          '100%': { 'background-position': '0 0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        glowing: 'glowing 20s linear infinite',
       },
     },
   },
