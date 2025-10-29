375031c0927bb6af8bbbe3da68eea1fb670cb55b
Mayank <mayanksharma4174@gmail.com>
2025-10-29 10:11:13 +0000

    Show nahi kar raha hai

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,7 +11,7 @@ type LogoProps = {
 export default function Logo({ onLinkClick }: LogoProps) {
   return (
     <Link href="/" onClick={onLinkClick} className="flex flex-col items-center gap-2" aria-label="DevAura Labs homepage">
-      <Image src="https://i.ibb.co/sVqVqS3/IMG-20251019-191415-1.png" alt="DevAura Labs Logo" width={200} height={200} className="rounded-lg" />
+      <Image src="https://i.ibb.co/sVqVqS3/IMG-20251019-191415-1.png" alt="DevAura Labs Logo" width={200} height={200} className="rounded-lg" unoptimized />
     </Link>
   );
 }
