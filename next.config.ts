f66a7a1eb4b22e697da3dea6096f0ad0ccde6b26
Mayank <mayanksharma4174@gmail.com>
2025-10-30 16:01:22 +0000

    Har type ka image loading all url no restriction or image related privac

--- next.config.ts
+++ next.config.ts
@@ -13,34 +13,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'beeimg.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
   experimental: {
