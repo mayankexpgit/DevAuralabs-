9ef016313ed9fb4845e80b16c57f1fc1889acb2b
Mayank <mayanksharma4174@gmail.com>
2025-10-31 07:32:01 +0000

    Vertically nahi horizontal hi rakho or slide karne ka feature do image k

--- src/components/sections/showcase-section.tsx
+++ src/components/sections/showcase-section.tsx
@@ -4,6 +4,13 @@
 import Image from 'next/image';
 import { showcaseImages } from '@/lib/showcase-data';
 import { cn } from '@/lib/utils';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 const ShowcaseCard = ({
   img,
@@ -17,15 +24,15 @@ const ShowcaseCard = ({
   return (
     <div
       className={cn(
-        'relative w-full aspect-square cursor-pointer overflow-hidden p-4',
+        'relative w-full aspect-video cursor-pointer overflow-hidden p-4',
         className
       )}
     >
       <div className="flex flex-row items-center justify-center gap-2 w-full h-full">
         <Image
           className="object-contain"
-          width={300}
-          height={300}
+          width={400}
+          height={400}
           alt={alt}
           src={img}
           unoptimized
@@ -38,14 +45,26 @@ const ShowcaseCard = ({
 export default function ShowcaseSection() {
     return (
       <section id="showcase" className="py-12 md:py-16">
-        <div className="relative w-full overflow-hidden bg-transparent">
-          <div className="container mx-auto px-4">
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 items-center">
-              {showcaseImages.map((image, index) => (
-                <ShowcaseCard key={index} img={image.url} alt={image.alt} />
-              ))}
-            </div>
-          </div>
+        <div className="container mx-auto px-4">
+           <Carousel
+                opts={{
+                    align: 'center',
+                    loop: true,
+                }}
+                className="w-full"
+            >
+                <CarouselContent>
+                    {showcaseImages.map((image, index) => (
+                        <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/1">
+                            <div className="p-1">
+                                <ShowcaseCard img={image.url} alt={image.alt} />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </div>
       </section>
     );
